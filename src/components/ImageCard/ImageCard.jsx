fd523262846add9dbdfaea95144b49f9a5491c20
GalinaKucherenko <galina.mail.k.n@gmail.com>
2024-07-07 10:09:20 +0300

    fixed the errors

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,7 +1,9 @@
 export default function ImageCard({ image, onImageClick }) {
     return (
-        <div onClick={() => onImageClick(image.url, image.alt_description || 'Image')}>
-            <img src={image.url} alt={image.alt_description || 'Image'} />
+        <div >
+            <img
+                onClick={() => onImageClick(image.url, image.alt_description || 'Image')}
+                src={image.url} alt={image.alt_description || 'Image'} />
         </div>
     );
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,20 +1,38 @@
 import { Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 import css from "./SearchBar.module.css";
+
+const validationSchema = Yup.object().shape({
+    topic: Yup.string()
+        .min(3, "Minimum 3 letters")
+        .required("This field is required"),
+});
 
 export default function SearchBar({ onSearch }) {
     return (
         <div className={css.formik}>
             <Formik
                 initialValues={{ topic: "" }}
+                validationSchema={validationSchema}
                 onSubmit={(values, actions) => {
                     onSearch(values.topic);
                     actions.resetForm();
                 }}
             >
-                <Form className={css.form}>
-                    <Field className={css.input} type="text" name="topic" placeholder="Search images and photos" />
-                    <button className={css.btn} type="submit">Search</button>
-                </Form>
+                {({ errors, touched }) => (
+                    <Form className={css.form}>
+                        <Field 
+                            className={css.input} 
+                            type="text" 
+                            name="topic" 
+                            placeholder="Search images and photos" 
+                        />
+                        <button className={css.btn} type="submit">Search</button>
+                        {errors.topic && touched.topic ? (
+                            <div>{errors.topic}</div>
+                        ) : null}
+                    </Form>
+                )}
             </Formik>
         </div>
     );
